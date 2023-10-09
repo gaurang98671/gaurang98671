6338132d39184189607d6e8bd6b0ca700b282dfa
Gaurang Pawar <56935661+gaurang98671@users.noreply.github.com>
2023-10-09 21:46:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import datetime
 gifs = {
-    "mon": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTUxcDBnNGxuN2oxNXA3M2xkNnl1cGYxb2dnYzAxcHp6ZGxsemFscSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9GI7UlOQ6uU95v82q7/giphy.gif",
-    "tue": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGZiZDZqNmhxeTVyN3M1NXljc2VxNTUwN2VwNWEzNWFzZWRoZmgzcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/idkWREpGm89wwwwAES/giphy.gif",
+    "mon": "https://media.giphy.com/media/D8N4jDVEpZ2OCdGjpn/giphy.gif",
+    "tue": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
     "wed": "https://media.giphy.com/media/JZysnXgm2nRxMLm52q/giphy.gif",
     "thu": "https://media.giphy.com/media/VJ5pkHokJgV78qy70E/giphy.gif",
     "fri": "https://media.giphy.com/media/YRPVlInmP08agsbXQR/giphy.gif",
